2b74b21f572b4f1a76e36c562058e7b2eb059355
Salmane <salmane-ben@live.com>
2021-06-08 16:04:07 +0100

    no idea

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is post on
+        our <a href="google.com">forums</a>.
+        <br/>
+        <br/>
+        Thanks.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
